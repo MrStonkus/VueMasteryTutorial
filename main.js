ab46953ac7286d8c73ae9ddfb8c8207a945d24ea
Valdas Stonkus <mr.valdas.stonkus@gmail.com>
2019-02-25 19:33:49 +0200

    First

---
+++ main.js
@@ -0,0 +1,40 @@
+//@ts-ignore
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Kojinės",
+    image: "./images/vmSocks-blue-onWhite.png",
+    altText: "Viena pora kojinių",
+    inStock: 1,
+    onSale: true,
+    details: [
+      "80% medvilnė",
+      "20% polisterolas",
+      "spalva saulės poveikiui atspari"
+    ],
+    variants: [
+      {
+        variantId: 111,
+        variantColor: 'žalia',
+        variantImage: './images/vmSocks-green-onWhite.png'
+      },
+      {
+        variantId: 112,
+        variantColor: 'mėlyna',
+        variantImage: './images/vmSocks-blue-onWhite.png'
+      },
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart++
+    },
+    removeFromCart() {
+      this.cart--;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    }
+  }
+});
